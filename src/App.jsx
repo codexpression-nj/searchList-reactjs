cf55396f4d243ce994d285c38d66108062141062
codexpression-nj <n.codexpression@gmail.com>
2023-09-21 23:47:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [results,setResults] = useState([]);
+
+  return (
+  <div className='App'>
+    <div className='searbar-container'>
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
